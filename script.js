f795c7fceb85c92084b2bc617b58a80d32d50f75
Noor infortech & Software Solutions <noorinfotech.co.in@gmail.com>
2026-06-02 20:40:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+fetch("data/blocks.json")
+  .then(res => res.json())
+  .then(blocks => {
+    const grid = document.getElementById("blocks-grid");
 
+    blocks.forEach(block => {
+      const card = document.createElement("div");
+
+      card.className = "block-card";
+
+      card.innerHTML = `
+        <img src="${block.preview}" alt="${block.title}">
+        <h3>${block.title}</h3>
+        <p>${block.category}</p>
+
+        <button data-id="${block.id}">
+          Open
+        </button>
+      `;
+
+      grid.appendChild(card);
+    });
+  });
